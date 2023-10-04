bc21fe86b3ec1acb6f9f96fc989dd4399f7a7b73
adjoawadee <adjoa.wadee@gmail.com>
2023-10-04 11:58:15 +0200

    initial set up

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Amplify } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 import { AppModule } from './app/app.module';
 
